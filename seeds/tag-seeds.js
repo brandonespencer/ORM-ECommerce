f9c822e9348149ec1af733bbeb2a29288602dbc3
Brandon Spencer <46231696+brandonespencer@users.noreply.github.com>
2022-04-14 01:09:18 -0700

    Update tag-seeds.js

--- seeds/tag-seeds.js
+++ seeds/tag-seeds.js
@@ -2,13 +2,13 @@ const { Tag } = require('../models');
 
 const tagData = [
   {
-    tag_name: 'rock music',
+    tag_name: 'rap music',
   },
   {
-    tag_name: 'pop music',
+    tag_name: 'r&b music',
   },
   {
-    tag_name: 'blue',
+    tag_name: 'yellow',
   },
   {
     tag_name: 'red',
@@ -20,10 +20,10 @@ const tagData = [
     tag_name: 'white',
   },
   {
-    tag_name: 'gold',
+    tag_name: 'black',
   },
   {
-    tag_name: 'pop culture',
+    tag_name: 'hip hop culture',
   },
 ];
 
