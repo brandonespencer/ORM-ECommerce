a257ff14c782c71dad190ad0ccc14b8644137671
Brandon Spencer <46231696+brandonespencer@users.noreply.github.com>
2022-04-13 18:33:25 -0700

    Update product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -22,7 +22,7 @@ router.get('/:id', async (req, res) => {
   try { 
     const productData = await Product.findByPk(req.params.id, { include: ({ model: Category }, {model: Tag }) });
     if (!productData) {
-    res.status(404).json({ message: "No product with this id"})
+    res.status(404).json({ message: "No product with this dpci"})
     return;
     }
     res.status(200).json(productData);
